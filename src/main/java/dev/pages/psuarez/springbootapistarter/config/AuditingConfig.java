0650fa3d309ee48a1e9f62838971efa031b8386d
Pablo Suárez <104940521+psuarezdev@users.noreply.github.com>
2024-12-21 19:35:42 +0000

    Create AuditingConfig.java

---
+++ src/main/java/dev/pages/psuarez/springbootapistarter/config/AuditingConfig.java
@@ -0,0 +1,8 @@
+package dev.pages.psuarez.springbootapistarter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {}
